9f148e8c535d269f48cf857b738962f023d3ebcb
elysium00039 <121762011+elysium00039@users.noreply.github.com>
2024-09-22 11:34:08 +0000

    up

--- wp-content/themes/blogone/template-parts/entry/entry-slider-content.php
+++
@@ -1,3 +0,0 @@
-<p>
-<?php blogone_the_excerpt_max_words(13, '', true, '...'); ?>
-</p>
--- wp-content/themes/blogone/template-parts/entry/meta-tags.php
+++
@@ -1,6 +0,0 @@
-<?php if( has_tag() ){ ?>
-<div class="bs-blog-category blog_tags">
-	<span class="tags"><?php _e('Tags:','blogone') ?></span>
-	<?php the_tags( '', get_the_ID() ); ?>
-</div>
-<?php } ?>
